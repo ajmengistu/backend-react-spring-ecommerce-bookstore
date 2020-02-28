97f23fcd62b402d05c1aecb25211dbe055e8f1c2
ajmengistu <a_mengistu@outlook.com>
2020-02-27 19:23:01 -0500

    Added a global CrossOrigins ocnfiguration to allow only frontend app url origins for development and production

--- src/main/java/com/ecommerce/bookstore/BookstoreApplication.java
+++ src/main/java/com/ecommerce/bookstore/BookstoreApplication.java
@@ -2,15 +2,28 @@ package com.ecommerce.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@CrossOrigin(origins = { "http://localhost:3000", "https://frontend-bookstore-react-sp.herokuapp.com",
-		"https://frontend-bookstore-react-spring.netlify.com" })
 @RestController
 @SpringBootApplication
 public class BookstoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+	// Global CrossOrigins configuration
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000",
+						"https://frontend-bookstore-react-sp.herokuapp.com",
+						"https://frontend-bookstore-react-spring.netlify.com");
+			}
+		};
 	}
 }
